742c56acac8e271acd451c5af0b196d6d3ad36db
Ole Jacob Mellgren <olejme@stud.ntnu.no>
2024-07-27 16:38:41 +0200

    minor changes

--- src/app/components/home.tsx
+++ src/app/components/home.tsx
@@ -46,11 +46,13 @@ function Home({ id, onChange }: HomeProps) {
             repeat={Infinity}
           />
           <p className="text-[#8892b0] text-lg py-4 max-w-[700px]">{data.about}</p>
-          <button className="text-white group border-2 px-6 py-3 my-2 flex items-center rounded-xl hover:bg-[#E25822] hover:border-[#E25822]"> <Link to="projects" smooth={true} offset={-80} duration={500}> View work </Link>
+          <Link to="projects" smooth={true} offset={-80} duration={500}>
+          <button className="text-white group border-2 px-6 py-3 my-2 flex items-center rounded-xl hover:bg-[#E25822] hover:border-[#E25822]">  View work 
             <span className="group-hover:rotate-90 duration-300">
               <HiArrowNarrowRight className="ml-3" />
             </span>
           </button>
+          </Link>
         </div>
       </div>
     </div>
